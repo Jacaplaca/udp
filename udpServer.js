476d5a67c1c3ccebfa769e9f70ea16480e25ef3a
Jacaplaca <dziewanowski@gmail.com>
2021-12-02 10:38:32 +0100

    sendSms turning on

--- udpServer.js
+++ udpServer.js
@@ -14,7 +14,7 @@ server.on('error', (err) => {
 server.on('message', (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
   sendEmail(msg)
-  // sendSMS(msg)
+  sendSMS(msg)
 });
 
 server.on('listening', () => {
